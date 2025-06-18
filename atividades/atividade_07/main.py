6eb42d52bfaf32aedd4f7f3ba8844bc7eb60777f
samu4alves <alvessamuel5000@gmail.com>
2025-06-17 22:02:26 -0300

    Atualizações

--- atividades/atividade_07/main.py
+++ atividades/atividade_07/main.py
@@ -1,42 +1,54 @@
-chaves = ('Nome', 'Data de Nascimento', 'Email', 'Cpf', 'Telefone', 'Profissão', 'Gênero')
+chaves = ('nome', 'data de nascimento', 'email', 'CPF', 'telefone', 'profissao', 'genero')
 
 pessoas = []
 
-# inserindo pessoa
 while True:
-    nova_pessoa = {}
-    menu = input('Escolha uma opção:')
+    pessoa = {}
+    menu = input("O que deseja fazer:\n 1- Inserir dados do novo usuario\n 2- Exibir lista de usuario\n 3- Alterar dados de um usuario\n 4- Excluir usuario da lista\n 5- Sair do programa\n")
     match menu:
-        case '1':
-            try:
-                for chave in chaves:
-                    if chave == "Idade":
-                        nova_pessoa[chave] = int(input('Informe a idade: '))
-                    else:
-                        nova_pessoa[chave] = input(f'Informe {chave}:')
-                pessoas.append(nova_pessoa)
-                print(f'{nova_pessoa.get(chaves[0])} inserida com sucesso')
-
-            except Exception as e:
-                print(f'Não foi possível cadastrar nova pessoa {e}')
-            # exibindo a nova lista
-        case '2':
-            for nova_pessoa in pessoas:
-                print(f'{'-'*40}')
-                for chave in chaves:
-                    print(f'{chave}: {nova_pessoa.get(chave)}')
-        case '3':
-            for i, nova_pessoa in enumerate(pessoas):
-                    print(f'Posição{i}: {nova_pessoa.get('Nome')}')
+        case "1":
+            for chave in chaves:
+                if chave == 'idade':
+                    pessoa[chave] = int(input(f"Digite {chave}: "))
+                else:
+                    pessoa[chave] = input(f"Digite {chave}: ")
+            pessoas.append(pessoa)
             
-            i = int(input('Informe o número do índice a ser alterado: '))
-            if i >= 0 and i < len(pessoas):
-                    chave = input('Chave que deseja alterar? Nome, Email...')
+        case "2":
+            for pessoa in pessoas:
+                print(f"{'-'*40}")
+                for chave in chaves:
+                    print(f"{chave.capitalize()}: {pessoa.get(chave)}")
+                print(f"{'-'*40}")
+
+        case "3":
+            for i, pessoa in enumerate(pessoas):
+                print(f"{i}: {pessoa.get('nome')}")
+            
+
+            try:
+                i = int(input("Digite o índice do usuário que deseja alterar: "))
+                if i >= 0 and i < len(pessoas):
+                    chave = input("Digite a chave que deseja alterar (ex: nome, email, etc): ")
                     if chave in chaves:
-                         novo_valor = input(f'Novo valor para chave')
-                         pessoas[i][chaves]
-                    print('Pessoa alterada com sucesso\n')
-            else:
-                print('Valor do índice inválido')
-            for i, nova_pessoa in enumerate(pessoas):
-                    print(f'Posição{i}: {nova_pessoa.get('Nome')}')
+                        novo_valor = input(f"Digite o novo valor para '{chave}': ")
+                        pessoas[i][chave] = novo_valor
+                        print("Dados atualizados com sucesso.")
+                    else:
+                        print("Chave inválida.")
+                else:
+                    print("Índice fora do intervalo.")
+            except Exception as e:
+                print(f"Você precisa digitar um número válido para o índice. {e}")
+
+        case "4":
+            for i, pessoa in enumerate(pessoas):
+                print(f"{i}: {pessoa.get('nome')}")
+
+            delete = int(input("Digite o dicionario que deseja deletar"))
+            if delete in range(len(pessoas)):
+                del pessoas[delete]
+                print("Pessoa deletada com sucesso")
+        
+        case "5":
+            break
